c62717f84dffeea9d64337f1100da30b04b4f651
tiansaipeng <835858151@qq.com>
2022-06-18 11:53:13 +0800

    d

--- Assets/Scripts/UI/AD/GameADView.cs
+++ Assets/Scripts/UI/AD/GameADView.cs
@@ -10,7 +10,7 @@ public class GameADView : Popuper
     // Start is called before the first frame update
     void Start()
     {
-        //test
+        //test 
     }
 
     // Update is called once per frame
