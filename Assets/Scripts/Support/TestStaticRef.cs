cdfa0a96010e7a0bb2cf50e828744993affb38a2
tiansaipeng <835858151@qq.com>
2022-06-26 12:12:55 +0800

    editor study

---
+++ Assets/Scripts/Support/TestStaticRef.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestStaticRef : MonoBehaviour
+{
+    //public static GameObject prefab;
+    //private void Start()
+    //{
+    //    prefab = Resources.Load<GameObject>("prefab");
+    //}
+}
