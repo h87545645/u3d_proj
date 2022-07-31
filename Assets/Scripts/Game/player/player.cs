4bf81da648c89570e4e5858da05d09a58a45c858
h87545645 <835858151@qq.com>
2022-07-31 11:05:58 +0800

    sprite

--- Assets/Scripts/Game/player/player.cs
+++ Assets/Scripts/Game/player/player.cs
@@ -7,7 +7,21 @@ public class player : MonoBehaviour
     public SpriteRenderer heroRenderer;
     public Rigidbody2D heroRigidbody2D;
 
-    private void Update()
+    private void Start()
+    {
+        //CollisionListerner.onCollisionEnter2D.AddListener(delegate (GameObject g1, GameObject g2) {
+        //    Debug.LogFormat("{0}¿ªÊ¼Åö×²{1}", g1.name, g2.name);
+        //});
+        //CollisionListerner.onCollisionStay2D.AddListener(delegate (GameObject g1, GameObject g2) {
+        //    Debug.LogFormat("{0}Åö×²ÖÐ{1}", g1.name, g2.name);
+        //});
+        //CollisionListerner.onCollisionExit2D.AddListener(delegate (GameObject g1, GameObject g2) {
+        //    Debug.LogFormat("{0}½áÊøÅö×²{1}", g1.name, g2.name);
+        //});
+    }
+
+
+    private void LateUpdate()
     {
         if (Input.GetKey(KeyCode.W))
         {
@@ -31,5 +45,30 @@ public class player : MonoBehaviour
     {
         heroRenderer.flipX = flipx;
         heroRigidbody2D.position += (position * 0.1f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            Debug.DrawLine(contact.point, transform.position, Color.red);
+            var direction = transform.InverseTransformPoint(contact.point);
+            if (direction.x > 0f)
+            {
+                print("right collision");
+            }
+            if (direction.x < 0f)
+            {
+                print("left collision");
+            }
+            if (direction.y > 0f)
+            {
+                print("up collision");
+            }
+            if (direction.y < 0f)
+            {
+                print("down collision");
+            }
+        }
     }
 }
