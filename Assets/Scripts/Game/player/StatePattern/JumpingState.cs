011abc511ae8d9bc2a7e831069403452e40450b2
h87545645 <835858151@qq.com>
2022-10-10 21:18:47 +0800

    1

--- Assets/Scripts/Game/player/StatePattern/JumpingState.cs
+++ Assets/Scripts/Game/player/StatePattern/JumpingState.cs
@@ -13,12 +13,21 @@ public class JumpingState : BaseState
         _fragHore = frag;
         _fragHore.fragAnim.SetTrigger("jump-up");
         _fragHore.fragAnim.SetBool("standing", false);
-        float chargeVaule = (float)(300 * chargeTime);
-        chargeVaule = Mathf.Clamp(chargeVaule, 100,1000);
+        float chargeValue = (float)(300 * chargeTime);
+        float yValue = Mathf.Clamp(chargeValue, 100,600);
+        float xValue = Mathf.Clamp(chargeValue, 100, 150);
         //float dir = (float)frag.direction * force;
-        Vector2 force = new Vector2((float)frag.direction * chargeVaule, chargeVaule);
-        _fragHore.heroRigidbody2D.AddForce(Vector2.up * chargeVaule);
-        _fragHore.heroRigidbody2D.AddForce(frag.direction == Game_Direction.Left ? Vector2.left * chargeVaule :  Vector2.right * chargeVaule);
+        Vector2 force = new Vector2((float)frag.direction * xValue , yValue);
+        _fragHore.heroRigidbody2D.AddForce(force);
+        if (chargeValue < 300)
+        {
+            _fragHore.fragAnim.speed = 1.5f;
+        }
+        else
+        {
+            _fragHore.fragAnim.speed = 1.0f;
+        }
+        //_fragHore.heroRigidbody2D.AddForce(frag.direction == Game_Direction.Left ? Vector2.left * chargeVaule :  Vector2.right * chargeVaule);
         //_fragHore.heroRigidbody2D.velocity = force;
         Debug.Log("------------------------Heroine in JumpingState~!(½øÈëÌøÔ¾×´Ì¬£¡)");
     }
